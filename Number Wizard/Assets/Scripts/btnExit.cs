38a465a89961ebfaaca98a4ae9b8ed7f14ee664b
BrutalCoding <dhbreedeveld@gmail.com>
2017-04-09 14:19:24 +0200

    Added UI. Also Play and Quit buttons working.

---
+++ Number Wizard/Assets/Scripts/btnExit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnExit : MonoBehaviour {
+
+	// Use this for initialization
+	public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
